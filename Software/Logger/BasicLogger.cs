1a668fe5d0a8f18b3bbfb265e5afa1def357cc89
UchiTesing <uchitesting57@gmail.com>
2024-01-20 04:43:08 +0100

    Fix infinite loop upon listening Traces
    Makes sense that when you log any Trace and une Trace yourself to log, something gonna happen.
    
    Tried Debug.WriteLine and other means under the Trace. Seems to be aliases.

--- Software/Logger/BasicLogger.cs
+++ Software/Logger/BasicLogger.cs
@@ -58,11 +58,7 @@ namespace Software.Logger
 
         public void DisplayMessage(string message)
         {
-#if DEBUG
-            Debug.WriteLine(message);
-#else
-    Console.WriteLine(message);
-#endif
+            Console.WriteLine(message);
         }
     }
 }
